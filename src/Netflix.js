45c84b8faa191768586c6b537ecc598a503c7cdf
ch15 <ch15solito@gmail.com>
2023-02-17 19:36:02 +0800

    update first react clone app

--- src/Netflix.js
+++ src/Netflix.js
@@ -3,33 +3,19 @@ import img1 from './images/netFlix.png';
 export default function NavBar(){
 
     return <div>
-
         <div className='contaniner'>
-
-        <nav className="nav">
+    <nav className="nav">
         <a href="/" className="site-title"><img className='logo' src={img1}/></a>
-        <ul>
-            <li>
-                <a href="/romantic">Romantic</a>
-            </li>
-            <li>
-                <a href="/comedy">Comedy</a>
-            </li>
-            <li>
-                <a href="/horror">Horror</a>
-            </li>
-            <li>
-                <a href="/classic">Classic</a>
-            </li>
-            <li>
-                <a href="/true-story">True story</a>
-            </li>
-            <li>
-                <a href="/history">History</a>
-            </li>
+        <ul> 
+            <li><a href="/romantic">Romantic</a></li>
+            <li><a href="/comedy">Comedy</a></li>
+            <li><a href="/horror">Horror</a></li>
+            <li><a href="/classic">Classic</a></li>
+            <li><a href="/true-story">True story</a></li>
+            <li><a href="/history">History</a></li>
         </ul>
-    </nav>
 
+    </nav>
     <div className="video-content">
         <iframe className='video-items' width="400" height="250" src="https://www.youtube.com/embed/sNO4X__dagA" title="The Tinder Swindler | Official Clip - The Man With Many Names | Netflix" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <iframe className='video-items' width="400" height="250" src="https://www.youtube.com/embed/IE8HIsIrq4o" title="The Adam Project | Official Trailer | Netflix" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
@@ -38,16 +24,9 @@ export default function NavBar(){
         <iframe className='video-items' width="400" height="250" src="https://www.youtube.com/embed/BmllggGO4pM" title="THE GRAY MAN | Official Trailer | Netflix" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <iframe className='video-items' width="400" height="250" src="https://www.youtube.com/embed/SL9aJcqrtnw" title="DON’T LOOK UP | Official Teaser Trailer | Netflix" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
-       
-
         <footer>
-        <p>
-        **This is just a fake portfolio. All the projects and contact details
-        given are not real.
-      </p>
-      <p>
-        © Created for Future Tech BootCamp
-      </p>
+            <p> **This is just a fake portfolio. All the projects and contact details given are not real.</p>
+            <p> © Created for Future Tech BootCamp</p>
         </footer>
         </div>     
 </div>
